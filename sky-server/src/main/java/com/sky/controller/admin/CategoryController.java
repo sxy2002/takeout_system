397d79b833067c9ed4e553e7d679a5b5933d4e00
sxy2002 <1980659897@qq.com>
2025-02-17 09:39:17 +0800

    add Category

---
+++ sky-server/src/main/java/com/sky/controller/admin/CategoryController.java
@@ -0,0 +1,36 @@
+package com.sky.controller.admin;
+
+import com.sky.dto.CategoryDTO;
+import com.sky.result.Result;
+import com.sky.service.CategoryService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/admin/category")
+@Slf4j
+@Api(tags = "菜品分类相关接口")
+public class CategoryController {
+
+    @Autowired
+    private CategoryService categoryService;
+
+    /**
+     * 新增菜品分类
+     * @param categoryDTO
+     * @return
+     */
+    @PostMapping
+    @ApiOperation("分类新增")
+    public Result addCategory(@RequestBody CategoryDTO categoryDTO) {
+        log.info("分类新增{}", categoryDTO);
+        categoryService.addCategory(categoryDTO);
+        return Result.success();
+    }
+}
--- sky-server/src/main/java/com/sky/controller/admin/EmployeeController.java
+++ sky-server/src/main/java/com/sky/controller/admin/EmployeeController.java
@@ -147,4 +147,8 @@ public class EmployeeController {
         return Result.success();
     }
 
+//    public Result updatePassword(@RequestBody EmployeeDTO employeeDTO) {
+//
+//    }
+
 }
